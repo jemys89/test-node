cee199857b65ee440e5b61f192aa1147fcdff1c1
Jamal El Boutaibi Mohamed <jemys89@gmail.com>
2019-11-13 11:03:53 +0100

    add couchbase

--- index.js
+++ index.js
@@ -5,40 +5,91 @@ var express = require('express'),
 var app = express();
  
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
- 
+
+var colors = require('colors/safe');
+
+var couchbase = require('couchbase');
+const host = process.env.HOST_COUCHBASE_DEV;
+var cluster = new couchbase.Cluster('couchbase://'+host+'/');
+const bucketName = process.env.BUCKET_NAME_DEV;
+const password = process.env.PASSWORD_COUCHBASE_DEV;
+cluster.authenticate(bucketName, password);
+var bucket = cluster.openBucket(bucketName, function(err){
+  console.log(colors.green(err));
+  if(err){
+    console.log(colors.red('ERROR AL CONECTAR A couchbase', err));
+    return;
+  }
+
+  console.log(colors.green('CONECTADO A COUCHBASE'));
+});
+var N1qlQuery = couchbase.N1qlQuery;
+
  const threats = [
   {
       id: 1,
-      displayName: 'Honorio, tu tines tu vida y yo la mia',
-      necessaryPowers: ['ligon'],
+      displayName: 'Honorio',
+      necessaryPowers: ['JAVA'],
       img: 'tower.jpg',
       assignedHero: 0
   },
   {
       id: 2,
-      displayName: 'Sergio, Pues ya hemos comido!!',
-      necessaryPowers: ['Listo'],
+      displayName: 'Sergio',
+      necessaryPowers: ['JAVA'],
       img: 'mess.jpg',
       assignedHero: 0
   },
   {
       id: 3,
-      displayName: 'Juan, ninja developer',
-      necessaryPowers: ['ninja garden'],
+      displayName: 'Juan',
+      necessaryPowers: ['JAVA'],
       img: 'joke.jpg',
       assignedHero: 0
   },
   {
       id: 3,
-      displayName: 'Jamal, negado',
-      necessaryPowers: ['Valeee....'],
+      displayName: 'Jamal',
+      necessaryPowers: ['JAVA'],
       img: 'joke.jpg',
       assignedHero: 0
   }
 ];
+
+
+
 app.get('/', function(req, res) {
-	console.log('buscando JAVA WEB');
-    res.send(threats);
+    return res.status(200).json({
+                    ok: false,
+                    team: threats,
+                    request: req.url
+                });
+});
+
+app.get('/couchbase/:id', function(req, res) {
+    var response = [];
+    var id = req.params.id;
+    var query_string = "SELECT count(*) as count FROM` +" bucketName +" WHERE type = 'statHit' and objectType = 'hotel-vst' and  objectID = $1";
+    console.log('id', id);
+    bucket.query(couchbase.N1qlQuery.fromString(query_string), [id], (error, rows) => {
+
+    if(error){
+      return res.status(500).json({
+        ok: false,
+        error
+      })
+    }
+     let count = rows[0].count;
+     return res.status(200).json({
+                    ok: true,
+                    count,
+                    request: req.url
+               });
+      
+
+    });;
+
+    
 });
 
 app.get('/:id', function(req, res) {
